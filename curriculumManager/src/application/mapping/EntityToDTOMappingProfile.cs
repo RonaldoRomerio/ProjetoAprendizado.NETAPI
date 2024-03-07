d997a082bf26abe1ce07eb924ba6ad90b66abc64
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2024-03-07 18:44:49 -0300

    Automapper
    Create a class to treat DTOs conversion

--- curriculumManager/src/application/mapping/EntityToDTOMappingProfile.cs
+++ curriculumManager/src/application/mapping/EntityToDTOMappingProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using curriculumManager.src.domain.dtos.customer;
+using curriculumManager.src.domain.dtos.education;
+using curriculumManager.src.domain.dtos.experience;
 using curriculumManager.src.domain.dtos.user;
 using curriculumManager.src.domain.models;
 
@@ -11,6 +14,16 @@ namespace curriculumManager.src.application.mapping
             CreateMap<User, InsertUser>().ReverseMap();
             CreateMap<User, LoginUser>().ReverseMap();
             CreateMap<User, LoggedUser>().ReverseMap();
+
+            CreateMap<Customer, CustomerWithPhoto>().ReverseMap();
+            CreateMap<Customer, CustomerInsert>().ReverseMap();
+            CreateMap<Customer, CustomerWithId>().ReverseMap();
+
+            CreateMap<Experience, ExperienceInsert>().ReverseMap();
+            CreateMap<Experience, ExperienceWithId>().ReverseMap();
+
+            CreateMap<Education, EducationInsert>().ReverseMap();
+            CreateMap<Education, EducationWithId>().ReverseMap();
         }
     }
 }
