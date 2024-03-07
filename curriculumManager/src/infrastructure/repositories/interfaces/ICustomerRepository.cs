34e4eac5d3835735da3cfa2e297773e4f60fa01a
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2024-03-07 18:36:30 -0300

    Crud
    Creating Repositories Interfacer

---
+++ curriculumManager/src/infrastructure/repositories/interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using curriculumManager.src.domain.models;
+
+namespace curriculumManager.src.infrastructure.repositories.interfaces
+{
+    public interface ICustomerRepository
+    {
+        public Task<Customer> insertAsync(Customer customer);
+        public Task<Customer> UpdateAsync(Customer customer);
+        public Task<int> DeleteAsync(int id);
+        public Task<Customer> getByIdAsync(int id);
+        public Task<List<Customer>> selectAll(int page);
+        public Task<bool> hasNextPage(int page);
+    }
+}
---
+++ curriculumManager/src/infrastructure/repositories/interfaces/IEducationRepository.cs
@@ -0,0 +1,13 @@
+﻿using curriculumManager.src.domain.models;
+
+namespace curriculumManager.src.infrastructure.repositories.interfaces
+{
+    public interface IEducationRepository
+    {
+        public Task<Education> insertAsync(Education education);
+        public Task<Education> UpdateAsync(Education education);
+        public Task<int> DeleteAsync(int id);
+        public Task<Education> getByIdAsync(int id);
+        public Task<List<Education>> selectAll(int page);
+    }
+}
