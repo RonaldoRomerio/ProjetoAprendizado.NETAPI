76e309d4c703f0e3bc867ec78afd053c7d8ac7d0
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2024-03-10 16:18:01 -0300

    Creating base classes
    Creating BaseCrudRepository

---
+++ curriculumManager/src/infrastructure/repositories/abstract/BaseCrudRepository.cs
@@ -0,0 +1,74 @@
+﻿using curriculumManager.src.domain.models;
+using curriculumManager.src.infrastructure.database.config;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace curriculumManager.src.infrastructure.repositories
+{
+    public abstract class BaseCrudRepository<TEntity>
+        where TEntity : class, new()
+    {
+        protected AppDbContext _context;
+        protected DbSet<TEntity> _dbSet;
+
+        public BaseCrudRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public virtual async Task<int> DeleteAsync(int id)
+        {
+            TEntity objectToUpdate = await getByIdAsync(id);
+
+            if (objectToUpdate == null)
+                throw new KeyNotFoundException("id não encontrado");
+
+            objectToUpdate.GetType().GetProperty("Deleted_at").SetValue(objectToUpdate, DateTime.UtcNow);
+
+            await _context.SaveChangesAsync();
+
+            return (int)objectToUpdate.GetType().GetProperty("Id").GetValue(objectToUpdate);
+        }
+
+        public virtual async Task<TEntity> getByIdAsync(int id)
+        {
+
+            var entityReturned = await _dbSet
+                .Where(x =>
+                    EF.Property<int?>(x, "Id") == id &&
+                    EF.Property<DateTime?>(x, "Deleted_at") == null
+                ).FirstOrDefaultAsync();
+
+            return entityReturned;
+        }
+
+        public virtual async Task<TEntity> insertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public virtual async Task<List<TEntity>> selectAll(int customerId)
+        {
+
+            PropertyInfo idProperty = typeof(TEntity).GetProperty("Id");
+            PropertyInfo deletedAtProperty = typeof(TEntity).GetProperty("Deleted_at");
+
+            return await _dbSet
+                .Where(x => EF.Property<DateTime?>(x, "Deleted_at") == null)
+                .AsNoTracking()
+                .OrderBy(x => EF.Property<int>(x, "Id"))
+                .ToListAsync();
+        }
+
+        public virtual async Task<TEntity> UpdateAsync(TEntity objectToUpdate)
+        {
+            _dbSet.Update(objectToUpdate);
+            await _context.SaveChangesAsync();
+
+            return objectToUpdate;
+        }
+    }
+}
